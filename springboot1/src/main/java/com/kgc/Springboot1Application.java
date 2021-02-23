7e041dbc8675941392f5d55e64cf82fbdfd73858
CY-ZZS <821971256@qq.com>
2021-02-23 11:32:00 +0800

    devupdept

--- springboot1/src/main/java/com/kgc/Springboot1Application.java
+++ springboot1/src/main/java/com/kgc/Springboot1Application.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.PropertySource;
 @SpringBootApplication
 @PropertySource("classpath:/myconfig/myconfig.properties")
 public class Springboot1Application {
-
+        //dev修改
     public static void main(String[] args) {
         SpringApplication.run(Springboot1Application.class, args);
     }
